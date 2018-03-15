ee3dc30a1bae601e872bf27c34bf1b1f5f5b5988
姚美君 <yaomeijun@hnjing.com>
2018-03-15 17:00:03 +0800

    添加src文件

---
+++ src/sw.js
@@ -0,0 +1,6 @@
+// self.addEventListener('fetch', function (event) {
+//   if (/\.jpg$/.test(event.request.url)) {
+//     event.respondWith(fetch('http://d.hiphotos.baidu.com/image/pic/item/09fa513d269759ee560ce9d8befb43166d22df41.jpg'))
+//   }
+// })
+console.log('4222')
